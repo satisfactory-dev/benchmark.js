67621243fbb7043967387e6974b53c9172b21d38
SignpostMarv <me@signpostmarv.name>
2026-01-19 19:23:01 +0000

    refactor to remove _.delay()

--- benchmark.js
+++ benchmark.js
@@ -555,7 +555,7 @@
      * @param {Object} fn The function to execute.
      */
     function delay(bench, fn) {
-      bench._timerId = _.delay(fn, bench.delay * 1e3);
+      bench._timerId = root.setTimeout(() => fn(), bench.delay * 1e3);
     }
 
     /**
