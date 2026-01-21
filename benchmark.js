7416e2fd3aa4c253ec979663757e04e0a90cbfbc
SignpostMarv <me@signpostmarv.name>
2026-01-21 06:27:20 +0000

    adjusting indentation

--- benchmark.js
+++ benchmark.js
@@ -234,19 +234,19 @@
        */
       static get decompilation() {
         if (this.#decompilation == undefined) {
-      try {
-        // Safari 2.x removes commas in object literals from `Function#toString` results.
-        // See http://webk.it/11609 for more details.
-        // Firefox 3.6 and Opera 9.25 strip grouping parentheses from `Function#toString` results.
-        // See http://bugzil.la/559438 for more details.
+          try {
+            // Safari 2.x removes commas in object literals from `Function#toString` results.
+            // See http://webk.it/11609 for more details.
+            // Firefox 3.6 and Opera 9.25 strip grouping parentheses from `Function#toString` results.
+            // See http://bugzil.la/559438 for more details.
             this.#decompilation = Function(
-          ('return (' + (function(x) { return { 'x': '' + (1 + x) + '', 'y': 0 }; }) + ')')
-          // Avoid issues with code added by Istanbul.
-          .replace(/__cov__[^;]+;/g, '')
-        )()(0).x === '1';
-      } catch {
+              ('return (' + (function(x) { return { 'x': '' + (1 + x) + '', 'y': 0 }; }) + ')')
+              // Avoid issues with code added by Istanbul.
+              .replace(/__cov__[^;]+;/g, '')
+            )()(0).x === '1';
+          } catch {
             this.#decompilation = false;
-      }
+          }
         }
 
         return this.#decompilation;
