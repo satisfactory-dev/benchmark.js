40e6689c71c9540277bbef255b07a35fdd21c5ed
SignpostMarv <me@signpostmarv.name>
2026-01-21 06:26:44 +0000

    refactor support to static class getters

--- benchmark.js
+++ benchmark.js
@@ -190,53 +190,76 @@
     var calledBy = {};
 
     /**
-     * An object used to flag environments/features.
+     * A class used to flag environments/features.
      *
-     * @static
      * @memberOf Benchmark
-     * @type Object
      */
-    var support = {};
+    class Support {
+      /**
+       * @type {boolean|undefined}
+       */
+      static #browser;
 
-    (function() {
+      /**
+       * @type {boolean|undefined}
+       */
+      static #decompilation;
 
       /**
        * Detect if running in a browser environment.
        *
-       * @memberOf Benchmark.support
-       * @type boolean
+       * @returns {boolean}
        */
-      support.browser = doc && isHostType(root, 'navigator');
+      static get browser() {
+        if (this.#browser == undefined) {
+          this.#browser = doc && isHostType(root, 'navigator');
+        }
+
+        return this.#browser;
+      }
 
       /**
        * Detect if the Timers API exists.
        *
-       * @memberOf Benchmark.support
-       * @type boolean
+       * @returns {boolean}
        */
-      support.timeout = true;
+      static get timeout() {
+        return true;
+      }
 
       /**
        * Detect if function decompilation is support.
        *
-       * @name decompilation
-       * @memberOf Benchmark.support
-       * @type boolean
+       * @returns {boolean}
        */
+      static get decompilation() {
+        if (this.#decompilation == undefined) {
       try {
         // Safari 2.x removes commas in object literals from `Function#toString` results.
         // See http://webk.it/11609 for more details.
         // Firefox 3.6 and Opera 9.25 strip grouping parentheses from `Function#toString` results.
         // See http://bugzil.la/559438 for more details.
-        support.decompilation = Function(
+            this.#decompilation = Function(
           ('return (' + (function(x) { return { 'x': '' + (1 + x) + '', 'y': 0 }; }) + ')')
           // Avoid issues with code added by Istanbul.
           .replace(/__cov__[^;]+;/g, '')
         )()(0).x === '1';
-      } catch(e) {
-        support.decompilation = false;
+      } catch {
+            this.#decompilation = false;
       }
-    }());
+        }
+
+        return this.#decompilation;
+      }
+    }
+
+    /**
+     * An object used to flag environments/features.
+     *
+     * @static
+     * @memberOf Benchmark
+     */
+    var support = Support;
 
     /**
      * Timer object used by `clock()` and `Deferred#resolve`.
